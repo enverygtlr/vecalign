04c529967482ed981b62a4711725690db458dc02
Enver Yigitler <enverygtlr@hotmail.com>
2024-02-05 15:55:03 +0300

    dp_core.pyx path changed.

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
 from Cython.Build import cythonize
 import numpy as np
+import os
 
 setup(
-    ext_modules = cythonize("dp_core.pyx"),
+    ext_modules = cythonize(os.path.join(os.environ['VEC'], "dp_core.pyx")),
     include_dirs=[np.get_include()]
 )
